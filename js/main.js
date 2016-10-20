0eb3e9df194a980cd338f4e21f5b9c489d075f6a
Matthew Harrington <mattharrington576@gmail.com>
2016-10-20 17:25:43 -0400

    Project complete

--- js/main.js
+++ js/main.js
@@ -2,32 +2,9 @@
     $(function() {
         "use strict";
 
-        // $(".navigation div span").on("click", function(){
-        //   if($(this).siblings("ul").css("display") == "block") {
-        //     $(this).siblings("ul").css("display", "none");
-        //   }
-        // });
-        //
-        // $(".navigation div span").on("click", function(){
-        //   if($(this).siblings("ul").css("display") == "none") {
-        //     $(this).siblings("ul").css("display", "block");
-        //   } else {
-        //     $(this).siblings("ul").css("display", "none");
-        //   }
-        // });
-
         $(".navigation div span").on("click", function(){
-          if($(this).siblings("ul").hasClass("dormant") == true) {
-            $(this).siblings("ul").slideToggle("fast", function(){
-              $(this).siblings("ul").removeClass("dormant");
-              $(this).siblings("ul").addClass("active");
-            });
-            } else if($(this).siblings("ul").hasClass("active") == true) {
-            $(this).siblings("ul").slideToggle("fast", function(){
-              $(this).siblings("ul").removeClass("active");
-              $(this).siblings("ul").addClass("dormant");
-            });
-          }
+          $('.dormant').slideUp("fast");
+          $(this).siblings("ul").slideDown("fast");
         });
 
     });
