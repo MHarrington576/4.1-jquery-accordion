3cb838bd3928f99050dd6bbf94cbd91bb87dd94d
Matthew Harrington <mattharrington576@gmail.com>
2016-10-20 15:37:52 -0400

    Slide animated

--- js/main.js
+++ js/main.js
@@ -2,11 +2,31 @@
     $(function() {
         "use strict";
 
-        $('.navigation div span').on('click', function(){
-          if($(this).siblings('ul').css("display")=="none"){
-            $(this).siblings('ul').css("display", "block");
-          } else {
-            $(this).siblings('ul').css("display", "none");
+        // $(".navigation div span").on("click", function(){
+        //   if($(this).siblings("ul").css("display") == "block") {
+        //     $(this).siblings("ul").css("display", "none");
+        //   }
+        // });
+        //
+        // $(".navigation div span").on("click", function(){
+        //   if($(this).siblings("ul").css("display") == "none") {
+        //     $(this).siblings("ul").css("display", "block");
+        //   } else {
+        //     $(this).siblings("ul").css("display", "none");
+        //   }
+        // });
+
+        $(".navigation div span").on("click", function(){
+          if($(this).siblings("ul").hasClass("dormant") == true) {
+            $(this).siblings("ul").slideToggle("fast", function(){
+              $(this).siblings("ul").removeClass("dormant");
+              $(this).siblings("ul").addClass("active");
+            });
+            } else if($(this).siblings("ul").hasClass("active") == true) {
+            $(this).siblings("ul").slideToggle("fast", function(){
+              $(this).siblings("ul").removeClass("active");
+              $(this).siblings("ul").addClass("dormant");
+            });
           }
         });
 
